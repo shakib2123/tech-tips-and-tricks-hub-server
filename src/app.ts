e3eab0a5566f2118d35c08130b1c6b75cff34b5e
shakib2123 <mshakibalhasan21@gmail.com>
2024-10-13 17:47:09 +0600

    local link changed

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "https://tech-tips-and-tricks-hub-server-omega.vercel.app",
+      "http://localhost:3000",
+    ],
     credentials: true,
   })
 );
